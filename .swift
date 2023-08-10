f52483473361a9f46e00b7e14074acf89aa8d01a
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-08-11 03:38:24 +0900

    Update and rename Swift to Swift.swift

---
+++ .swift
@@ -0,0 +1,12 @@
+var str = "hello,"
+str += " world"
+
+
+guard let leaseStart = aBuilding.TenantList[5]?.leaseDetails?.startDate else
+{
+    //handle the error case where anything in the chain is nil
+    //else scope must exit the current method or loop
+}
+//continue, knowing that leaseStart is not nil
+
+
